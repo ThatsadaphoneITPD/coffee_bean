ce427d9a048ea339bbccad44bd5a5880f9945c50
ThatsadaphoneITPD <dayeriveren@gmail.com>
2026-05-13 15:02:56 +0700

    update coffee 7

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import Providers from './providers';
+'use client';
+import { LayoutProvider } from '../layout/context/layoutcontext';
+import { PrimeReactProvider } from 'primereact/api';
 import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import '../styles/layout/layout.scss';
 import '../styles/demo/Demos.scss';
+import { Toaster } from 'react-hot-toast';
 
 interface RootLayoutProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link id="theme-css" href={`/themes/lara-light-blue/theme.css`} rel="stylesheet" />
-      </head>
-      <body>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <head>
+                <link id="theme-css" href={`/themes/lara-light-blue/theme.css`} rel="stylesheet"></link>
+            </head>
+            <body>
+                <PrimeReactProvider>
+                    <Toaster />
+                    <LayoutProvider>{children}</LayoutProvider>
+                </PrimeReactProvider>
+            </body>
+        </html>
+    );
 }
